5c8527751a36774417657cbb6c44cd64161b9008
ExistenceisP4ain <57858720+ExistenceisP4ain@users.noreply.github.com>
2020-04-23 10:32:13 +0200

    Update index.js

--- rest_mock/src/index.js
+++ rest_mock/src/index.js
@@ -8,27 +8,27 @@ const port = 3200; // port na kojem će web server slušati
 app.use(cors());
 app.use(express.json());
 
-// studenti
-app.get('/studenti', (req, res) => res.json(data.studenti));
-app.get('/studenti/:jmbag', (req, res) => res.json(data.jedan_student));
-app.post('/studenti', (req, res) => {
+// users
+app.get('/korisnici', (req, res) => res.json(data.users));
+app.get('/korisnici/:id', (req, res) => res.json(data.one_user));
+app.post('/korisnici', (req, res) => {
     res.statusCode = 201;
-    res.setHeader('Location', '/studenti/1234');
+    res.setHeader('Location', '/korisnici/1234');
     res.send();
 });
 
-// kolegiji
-app.get('/kolegiji', (req, res) => res.json(data.kolegiji));
+// oglasi
+app.get('/oglasi', (req, res) => res.json(data.oglasi));
 
-// ispiti
-app.get('/kolegiji/:kolegij/ispiti/:datum/rezultati', (req, res) => res.json(data.pretragaIspita));
-app.get('/kolegiji/:kolegij/ispiti/:datum/rezultati/student/:jmbag', (req, res) => res.json(data.ocjenaStudenta));
+// oglas
+app.get('/oglasi/:naslov/:oglas', (req, res) => res.json(data.pretragaOglasa));
+app.get('/oglasi/istekli/:oglas', (req, res) => res.json(data.istekliOglas));
 
-// unos jedne ocjene
-app.put('/kolegiji/:kolegij/ispiti/:datum/rezultati/student/:jmbag', (req, res) => {
+// unos oglasa
+app.put('/oglasi/:oglas', (req, res) => {
     let data = req.body;
 
-    if (!data.ocjena) {
+    if (!data.oglasid) {
         res.statusCode = 400;
         return res.json({
             error: 'There are parameters missing.',
@@ -36,7 +36,7 @@ app.put('/kolegiji/:kolegij/ispiti/:datum/rezultati/student/:jmbag', (req, res) 
     }
 
     res.statusCode = 201;
-    res.setHeader('Location', '/kolegiji/wapps/ispiti/01-05-2020/rezultati/student/003512341234');
+    res.setHeader('Location', '/oglasi/posao-u-infobipu/003512341234');
     res.send();
 });
 
